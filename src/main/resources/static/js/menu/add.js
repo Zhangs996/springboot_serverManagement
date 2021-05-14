fddfacc4a1bec624b837b3c5fa8761cc84b62cb5
zcl <2402568058@qq.com>
2021-05-14 22:21:50 +0800

    git修复

---
+++ src/main/resources/static/js/menu/add.js
@@ -0,0 +1,10 @@
+//自定义验证表单
+layui.use(['table', 'laydate', 'util', 'form'], function () {
+    var form=layui.form;
+    form.verify({
+        num: [
+            /^[1-9]\d*$/
+            , '密码必须6到12位，且不能出现空格'
+        ]
+    });
+})
